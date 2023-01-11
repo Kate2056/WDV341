df4332ec1d2ce9b9bfdee85dfd89131ea8162bde
Kaitlyn Briggs <krbriggs@dmacc.edu>
2023-01-11 13:35:59 -0600

    first commit

---
+++ wdv341.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WDV341 Homework Page</title>
+</head>
+<body>
+    <?php 
+        echo "<h1>WDV341 Homework Page<h1>";
+        echo "<h3>Intro PHP</h3>";
+    ?>
+</body>
+</html>
